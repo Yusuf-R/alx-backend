591262bf354cc418ec094891926d56541fe7cec0
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-25 15:57:44 +0100

    fix: :bug: ensure no positional changes

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -3,7 +3,7 @@
 from collections import OrderedDict, Counter
 
 
-BaseCaching = __import__('base_caching').BaseCaching
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 # Create a class LFUCache that inherits from BaseCaching system:
@@ -16,44 +16,62 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ Caching System """
+    """Caching System"""
 
     def __init__(self):
-        """ Initiliaze
-        """
+        """Initiliaze"""
         super().__init__()
         self.cache_data = OrderedDict(self.cache_data)
         self.frequency = Counter()
 
     def print_cache(self):
-        """ Print the cache content """
+        """Print the cache content"""
         print("Current cache:")
         for key, (item, _) in self.cache_data.items():
             print("{}: {}".format(key, item))
 
     def put(self, key, item):  # sourcery skip: extract-method, last-if-guard
-        """ Add an item in the cache """
+        """Add an item in the cache"""
         if key and item:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # find the mimimum frequency in the cache
                 min_freq = min(self.frequency.values())
+
+                # create a list of keys whose values matches minimum freq
                 items_to_discard = [
-                    k for k, v in self.frequency.items() if v == min_freq]
-                last_key = min(items_to_discard,
-                               key=lambda k: self.cache_data[k][1])
+                    k for k, v in self.frequency.items() if v == min_freq
+                ]
+                # item_to_discard = []
+                # for k, v in self.frequency.items():
+                #     if v == min_freq:
+                #         item_to_discard.append(k)
+
+                # within this list we need to find the last key item
+                last_key = min(
+                    items_to_discard, key=lambda k: self.cache_data[k][1]
+                )
 
                 del self.cache_data[last_key]
                 del self.frequency[last_key]
+
+                self.cache_data[key] = (item, 0)
+                # self.cache_data.move_to_end(key)
+                self.frequency[key] += 1
                 print("DISCARD: {}".format(last_key))
+                return
 
             self.cache_data[key] = (item, 0)
             self.frequency[key] += 1
 
     def get(self, key):
-        """ Get an item by key """
+        """Get an item by key"""
         if key in self.cache_data:
-            self.cache_data.move_to_end(key)
+            # self.cache_data.move_to_end(key)
+            # extract the value -> tuple(value, int(freq))
             item, freq = self.cache_data[key]
+            # increase the frequency for that key
             self.cache_data[key] = (item, freq + 1)
+            # store the key and its frequency in frequency dict
             self.frequency[key] += 1
             return item
         return None
