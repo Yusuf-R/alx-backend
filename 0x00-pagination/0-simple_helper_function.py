7b108093366e58cc648edc45afef10ffd38f6532
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-26 00:53:08 +0100

    feat: :sparkles: simple xcal for start and end index

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-""" Range simple helper fun """
-from typing import Tuple
+""" Pagination implementation"""
+# Write a function named index_range that takes
+# two integer arguments page and page_size.
+
+# The function return a tuple of size two containing a start index
+# and an end index corresponding to the range of indexes to return
+# in a list for those particular pagination parameters.
+
+# Page numbers are 1-indexed, i.e. the first page is page 1.
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
-        Range of the page
+    Return a tuple of size two containing the start and end
+    indexes to paginate a dataset.
 
-        Args:
-            page: Current page
-            page_size: Total size of the page
+    Args:
+      page (int): the current page number.
+      page_size (int): the number of items per page.
 
-        Return:
-            tuple with the range start and end size page
+    Returns:
+      tuple[int, int]: a tuple containing the start and end
+      indexes to paginate the dataset.
     """
-
-    final_size: int = page * page_size
-    start_size: int = final_size - page_size
-
-    return (start_size, final_size)
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
+    return (start_index, end_index)
