b233452825345d34784e31451f48217cc5510c57
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-25 16:54:51 +0100

    feat: :sparkles: esure LFU

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -33,38 +33,25 @@ class LFUCache(BaseCaching):
     def put(self, key, item):  # sourcery skip: extract-method, last-if-guard
         """Add an item in the cache"""
         if key and item:
-            # check if key already exist in the dictionary
             if key in self.cache_data:
-                # check if the number of item is higher than MAX_ITEMS
-                self.cache_data[key] = (item, 0)
+                # Update frequency for existing key
+                _, freq = self.cache_data[key]
                 self.frequency[key] += 1
+                self.cache_data[key] = (item, freq + 1)
                 return
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # find the mimimum frequency in the cache
-                min_freq = min(self.frequency.values())
 
-                # create a list of keys whose values matches minimum freq
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                min_freq = min(self.frequency.values())
                 items_to_discard = [
                     k for k, v in self.frequency.items() if v == min_freq
                 ]
-                # item_to_discard = []
-                # for k, v in self.frequency.items():
-                #     if v == min_freq:
-                #         item_to_discard.append(k)
-
-                # within this list we need to find the last key item
                 last_key = min(
-                    items_to_discard, key=lambda k: self.cache_data[k][1]
+                    items_to_discard, key=lambda k: self.frequency[k]
                 )
 
                 del self.cache_data[last_key]
                 del self.frequency[last_key]
-
-                self.cache_data[key] = (item, 0)
-                # self.cache_data.move_to_end(key)
-                self.frequency[key] += 1
                 print("DISCARD: {}".format(last_key))
-                return
 
             self.cache_data[key] = (item, 0)
             self.frequency[key] += 1
