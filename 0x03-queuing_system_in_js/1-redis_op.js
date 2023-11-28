e518fc565c811643838f6bf9f8868668fe1964ce
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-28 22:43:06 +0100

    feat: :sparkles: basic redix operation in node environment

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+import { createClient } from 'redis';
+
+const client = createClient();
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Reply: ${reply}`);
+  });
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
