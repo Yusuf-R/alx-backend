a19f6633dca6865fdd69173d250edd6e8d7e1789
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-28 23:21:11 +0100

    fix: :bug: added the client object event emitter messeages

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -4,6 +4,14 @@ const { createClient } = require('redis');
 
 const client = createClient();
 
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
 // Manually promisify the nedded functions in the client object
 client.getAsync = util.promisify(client.get).bind(client);
 client.setAsync = util.promisify(client.set).bind(client);
