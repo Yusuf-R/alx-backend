f145778e49494be75762a461721b8b7c7ccf76fe
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-28 22:30:14 +0100

    fix: :bug: needles to make it a function

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,16 +1,12 @@
 /* eslint-disable no-console */
 import { createClient } from 'redis';
 
-function redisServer() {
-  const client = createClient();
+const client = createClient();
 
-  client.on('connect', () => {
-    console.log('Redis client connected to the server');
-  });
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
 
-  client.on('error', (err) => {
-    console.log(`Redis client not connected to the server: ${err}`);
-  });
-}
-
-module.exports = redisServer;
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
